39e2207dfbaa89addecf52c3d111eff5dbe544f4
moka <gaoyuanfell@sina.com>
2017-06-06 18:03:09 +0800

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {UserComponent} from "./user/user.component";
+import {IndexComponent} from "./index/index.component";
 
 const routes: Routes = [
     {
         path: '',
-        children: []
+        redirectTo: '/index',
+        pathMatch: 'full',
+    },
+    {
+        path: 'user',
+        component:UserComponent
+    },
+    {
+        path: 'index',
+        component:IndexComponent
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {httpInterceptor} from "../providers/Interceptor";
 import {APP_BASE_HREF} from "@angular/common";
+import {UserComponent} from './user/user.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UserComponent,
+        IndexComponent
     ],
     imports: [
         BrowserModule,
