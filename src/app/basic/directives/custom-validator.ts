d52f0d0546c2d52489753acb01617424a6a1e3bb
moka <gaoyuanfell@sina.com>
2017-06-16 11:52:41 +0800

    gh

--- src/app/basic/directives/custom-validator.ts
+++
@@ -1,8 +0,0 @@
-import {AbstractControl, ValidatorFn} from "@angular/forms";
-export function customValidator(nameRe: RegExp): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} => {
-        const name = control.value;
-        const no = nameRe.test(name);
-        return no ? {'custom': {name}} : null;
-    };
-}
